f089b4fb1e0afcd32deffb4c4abd7f44c2815a8e
RAIN <534123074@qq.com>
2018-04-09 16:28:30 +0800

    bugfix

--- js/app/setting.js
+++ js/app/setting.js
@@ -1,16 +1,6 @@
 define(['mui', 'mall', 'updata'], function(mui, $, updata) {
 
 	mui.init({
-		preloadPages: [{
-			url: 'account.html',
-			id: 'account.html'
-		}, {
-			url: 'modify_mobile.html',
-			id: 'modify_mobile.html'
-		}, {
-			url: 'forget_password.html',
-			id: 'forget_password.html'
-		}],
 		swipeBack: true //启用右滑关闭功能
 	});
 
@@ -18,34 +8,10 @@ define(['mui', 'mall', 'updata'], function(mui, $, updata) {
 		console.log('#setting plusReady()');
 		webviewInit();
 		
-		mui.preload({
-			url: 'user_edit.html',
-			id: 'user_edit.html'
-		});
-		var versions = document.querySelector('#versions');
-			urlBaseP = plus.storage.getItem('urlBaseP');
-			updata.setUrl(urlBaseP);	
-		
-		if(plus.os.name == 'iOS') {
-			versions.style.backgroundColor = '#fff';
-		} else {
-			versions.addEventListener('tap', updata.updata);
-		}
-
 		//	获取当前版本号
 		plus.runtime.getProperty(plus.runtime.appid, function(appinfo) {
 			console.log(appinfo.version);
 			$.setValue('#version', appinfo.version);
-		});
-
-		//	跳转账号管理页面
-		$.tapHandler('.goAccount', 'account.html');
-
-		//	跳转修改信息页面
-		$.tapHandler({
-			selector: '.goSellerEdit',
-			url: 'user_edit.html',
-			event: 'getInfo'
 		});
 
 		//	退出app	
@@ -63,24 +29,6 @@ define(['mui', 'mall', 'updata'], function(mui, $, updata) {
 		});
 
 	});
-	
-	function delDeviceToken(deviceToken) {
-	urlBaseP = plus.storage.getItem('urlBaseP');
-	var url = urlBaseP + '/user/reset/devicetoken/' + deviceToken;
-	console.log('updateDeviceToken' + url);
-	$.put(url).then(function(data) {
-		console.log(JSON.stringify(data));
-		if(data.code === 0) {
-			mui.toast('成功');
-		} else {
-			mui.toast('出错了');
-		}
 
-	}).fail(function(status) {
-		statusHandler(status);
-		console.log('updateDeviceToken失败' + status)
-	});
-}
-	
 
 });
